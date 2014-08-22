9746483749b49e9eab53aa7c51c13b2f04383f4c
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-22 12:10:44 -0700

    show an individual lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,22 @@ class LessonsController < ApplicationController
 		@lessons = Lesson.all
 		render('lessons/index.html.erb')
 	end
+
+	def new
+		render ('lessons/new.html.erb')
+	end
+
+	def create
+		@lesson = Lesson.new(:name => params[:name], :information => params[:information])
+		if @lesson.save
+			redirect_to("/lessons")
+		else
+			render('lessons/new.html.erb')
+		end
+	end
+
+	def show
+		@lesson = Lesson.find(params[:id])
+		render('lessons/show.html.erb')
+	end
 end
