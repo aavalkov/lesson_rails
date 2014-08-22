1d9af37fc575abe4866ab86b5bd258209ceed2df
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-22 15:09:22 -0700

    allow users to number the lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -10,7 +10,7 @@ class LessonsController < ApplicationController
 	end
 
 	def create
-		@lesson = Lesson.new(:name => params[:name], :information => params[:information])
+		@lesson = Lesson.new(:name => params[:name], :information => params[:information], :number =>params[:number])
 		if @lesson.save
 			redirect_to("/lessons")
 		else
@@ -30,7 +30,7 @@ class LessonsController < ApplicationController
 
 	def update
 		@lesson = Lesson.find(params[:id])
-		@lesson.update(:name => params[:name], :information => params[:information])
+		@lesson.update(:name => params[:name], :information => params[:information], :number => params[:number])
 		redirect_to("/lessons/#{@lesson.id}")
 	end
 
