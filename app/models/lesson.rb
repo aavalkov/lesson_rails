5ff891e7e73d90a069a7427d8cd34ccb57570056
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-22 16:10:59 -0700

    allow users to go to the next lesson that is in numerical order

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,10 @@
 class Lesson < ActiveRecord::Base
  	validates :name, :presence => true
  	validates :information, :presence => true
+ 	validates :number, :presence => true
+
+ 	def next
+ 		Lesson.find_by(number: self.number+1)
+ 	end
+
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,4 +3,12 @@ require 'spec_helper'
 describe Lesson do 
 	it { should validate_presence_of :name }
  	it { should validate_presence_of :information }
+
+	context '#next' do
+  	it 'returns the lesson with the next-highest number than the current lesson' do
+    	current_lesson = Lesson.create({:name => 'lesson1', :information =>'info', :number => 1})
+    	next_lesson = Lesson.create({:name => 'lesson2', :information =>'info', :number => 2})
+    	current_lesson.next.should eq next_lesson
+  	end
+	end
 end
